ee5c179353ba09736d65766cc72f37cc22f1b7f4
Eduardo <eduardomell06@hotmail.com>
2025-07-07 03:23:44 -0300

    Organizing

--- Arquitetura de Aplicações Web com Rails/app/decorators/story_decorator.rb
+++
@@ -1,9 +0,0 @@
-class StoryDecorator < SimpleDelegator
-  def preview
-    body.truncate(20)
-  end
-
-  def status
-    published? ? "Publicado" : "Rascunho"
-  end
-end
--- Arquitetura de Aplicações Web com Rails/app/models/concerns/searchable.rb
+++
@@ -1,7 +0,0 @@
-module Searchable
-  extend ActiveSupport::Concern
-
-  included do
-    scope :search_by_title, ->(term) { where("title ILIKE ?", "%#{term}%") }
-  end
-end
